951e723e57feaf3e4686e6fb991af3e29e64cc00
ThiagoGenerali <thiagonmag@gmail.com>
2026-07-15 14:54:10 +0200

    Implement User Class

--- src/BankConsole/Models/User.cs
+++ src/BankConsole/Models/User.cs
@@ -8,5 +8,15 @@ namespace CSharpBankProject.src.BankConsole.Models
 {
     internal class User
     {
+        private Guid Id { get; init; }
+        public List<string> NameUsername { get; set; }
+        private string Password { get; set; }
+
+        public User(string name, string username, string password)
+        {
+            this.Id = Guid.NewGuid();
+            this.NameUsername = new List<string> { name, username };
+            this.Password = password;
+        }
     }
 }
