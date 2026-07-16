b44b737dd1f20b11b5cf52f9f63694a3c9a80dba
ThiagoGenerali <thiagonmag@gmail.com>
2026-07-16 23:11:33 +0200

    Change Id protection to be allowed to use in Account

--- src/BankConsole/Models/Account.cs
+++ src/BankConsole/Models/Account.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CSharpBankProject.src.BankConsole.Data;
+using CSharpBankProject.src.BankConsole.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace CSharpBankProject.src.BankConsole.Models
 {
-    internal class Account
+    internal class Account 
     {
+
     }
 }
--- src/BankConsole/Models/User.cs
+++ src/BankConsole/Models/User.cs
@@ -9,7 +9,7 @@ namespace CSharpBankProject.src.BankConsole.Models
 {
     internal class User : UserRepository
     {
-        private Guid Id { get; init; }
+        public Guid Id { get; init; }
         public List<string> NameUsername { get; set; }
         public string Password { get; private set; }
 
@@ -21,10 +21,14 @@ namespace CSharpBankProject.src.BankConsole.Models
         }
 
         public void UserLogin(string username, string password)
-        { }
+        {
+            //Will be called from the Login service and will call the UserRepository to check if the username and password match any existing user in the database.
+        }
 
         public void UserRegister(string name, string username, string password)
-        { }
+        {
+            //Will be called from the Register service and will call the UserRepository to add a new user to the database.
+        }
 
     }
 }
