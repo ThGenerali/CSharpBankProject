21b9801d1850911c806d1cf3352f7543ac580f8f
ThiagoGenerali <thiagonmag@gmail.com>
2026-07-22 19:41:12 +0200

    Fix logic in Verify Username. Modify User and UserRepository properties.

--- src/BankConsole/Models/User.cs
+++ src/BankConsole/Models/User.cs
@@ -7,27 +7,17 @@ using System.Threading.Tasks;
 
 namespace CSharpBankProject.src.BankConsole.Models
 {
-    internal class User : UserRepository
+    internal class User 
     {
-        public Guid Id { get; init; }
-        public List<string> NameUsername { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
         public string Password { get; private set; }
 
         public User(string name, string username, string password)
         {
-            this.Id = Guid.NewGuid();
-            this.NameUsername = new List<string> { name, username };
+            this.Name = name;
+            this.Username = username;
             this.Password = password;
-        }
-
-        public void UserLogin(string username, string password)
-        {
-            //Will be called from the Login service and will call the UserRepository to check if the username and password match any existing user in the database.
-        }
-
-        public void UserRegister(string name, string username, string password)
-        {
-            //Will be called from the Register service and will call the UserRepository to add a new user to the database.
         }
 
     }
