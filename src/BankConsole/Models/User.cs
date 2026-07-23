eb761b2490281bf1eb5e645ab17dd2ed93538e4f
ThiagoGenerali <thiagonmag@gmail.com>
2026-07-23 08:56:12 +0200

    Fix inheritance misuse. Create User class methods.

--- src/BankConsole/Models/User.cs
+++ src/BankConsole/Models/User.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,15 +10,43 @@ namespace CSharpBankProject.src.BankConsole.Models
 {
     internal class User 
     {
-        public string Name { get; set; }
-        public string Username { get; set; }
+        public Guid Id { get; init; }
+        public string Name { get; }
+        public string Username { get; init; }
         public string Password { get; private set; }
 
         public User(string name, string username, string password)
         {
+            this.Id = Guid.NewGuid();
             this.Name = name;
             this.Username = username;
             this.Password = password;
+        }
+
+        UserRepository userRepository = new UserRepository();
+
+        public void UpdatePassword(string newPassword, string currentPassword)
+        {
+            if (userRepository.VerifyPassword(this.Username, currentPassword))
+            {
+                this.Password = newPassword;
+            } else {
+                throw new UnauthorizedAccessException("Current password is incorrect. Password update failed.");
+            }
+        } 
+        
+        public void PrintUserInfo(string password)
+        {
+            if (userRepository.VerifyPassword(this.Username, password))
+            {
+                List<string> userInfo = userRepository.GetUserInfo(this.Id);
+                foreach (var info in userInfo)
+                {
+                    Console.WriteLine(info);
+                }
+            } else {
+                throw new UnauthorizedAccessException("Incorrect password. Cannot display user information.");
+            }
         }
 
     }
